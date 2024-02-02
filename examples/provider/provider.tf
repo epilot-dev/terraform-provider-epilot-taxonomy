7cc8e9a4cc6fb4b6575277aafce4fc3733c3d20e
Nishu Goel <n.goel@epilot.cloud>
2024-02-02 15:39:16 +0100

    Init Purpose TF Provider

---
+++ examples/provider/provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    epilot-purpose = {
+      source  = "epilot-dev/epilot-purpose"
+      version = "0.3.2"
+    }
+  }
+}
+
+provider "epilot-purpose" {
+  # Configuration options
+}
